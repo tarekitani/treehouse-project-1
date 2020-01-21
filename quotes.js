c04e680931f0240541472399c69268efd24abbcb
tarek-itani <44179992+tarekitani@users.noreply.github.com>
2020-01-20 19:08:06 -0800

    Initial commit

---
+++ quotes.js
@@ -0,0 +1,18 @@
+var quotes = [
+//temporarily only putting 3 quotes because timesink
+    {
+        quote: "The purpose of life, is a life of purpose.",
+        source: "Tarek itani",
+        year: 2007
+    },
+    {
+        quote: "In all cities, the better classes - the business men - are the sources of corruption, but they are so rarely pursued and caught that we do not fully realize whence the trouble comes.",
+        source: "Lincoln Steffens",
+        year: 1866
+    },
+    {
+        quote: "Everyone has noticed how hard it is to turn our thoughts to God when everything is going well with us... While what we call 'our own life' remains agreeable, we will not surrender it to Him. What, then, can God do in our interests but make 'our own life' less agreeable to us, and take away the plausible sources of false happiness?",
+        source: "C. S. Lewis",
+        year: 1963
+    }
+]
